c6b92f174669316aaf6f7797e5817686ffedd415
Michael Blix <mkeblx@gmail.com>
2015-06-20 18:25:52 -0700

    fragment usage

--- src/com/navigatevr/ytvr/MainActivity.java
+++ src/com/navigatevr/ytvr/MainActivity.java
@@ -3,32 +3,32 @@ package com.navigatevr.ytvr;
 import android.os.Bundle;
 
 import com.google.android.youtube.player.YouTubePlayer;
-import com.google.android.youtube.player.YouTubePlayerView;
+import com.google.android.youtube.player.YouTubePlayerFragment;
 
 public class MainActivity extends YouTubeFailureRecoveryActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.playerview);
 
-        YouTubePlayerView youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
-        youTubeView.initialize(DeveloperKey.DEVELOPER_KEY, this);
-
+        YouTubePlayerFragment youTubePlayerFragment =
+            (YouTubePlayerFragment) getFragmentManager().findFragmentById(R.id.youtube_fragment);
+        youTubePlayerFragment.initialize(DeveloperKey.DEVELOPER_KEY, this);
     }
 
     @Override
     public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer player,
         boolean wasRestored) {
-      if (!wasRestored) {
-        player.cueVideo("wKJ9KzGQq0w");
-      }
+        if (!wasRestored) {
+            player.cueVideo("Q8TXgCzxEnw");
+        }
     }
 
     @Override
     protected YouTubePlayer.Provider getYouTubePlayerProvider() {
-      return (YouTubePlayerView) findViewById(R.id.youtube_view);
+        return (YouTubePlayerFragment) getFragmentManager().findFragmentById(R.id.youtube_fragment);
     }
 
 }
---
+++ src/com/navigatevr/ytvr/VRActivity.java
@@ -0,0 +1,5 @@
+package com.navigatevr.ytvr;
+
+public class VRActivity {
+
+}
